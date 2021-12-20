fb781a1d79c8abd4ed7bb50b47ddf37e66e1466b
Kamil Peza <>
2021-12-20 16:17:55 -0500

    added lazy loading

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,17 +1,26 @@
+import React, { Suspense } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import About from "../Pages/About.js";
-import Reps from "../Pages/Representatives/Reps.js";
-import RepInfo from "../Pages/Representatives/RepInfo.js";
-import Senators from "../Pages/Senators/Senators.js";
-import SenatorInfo from "../Pages/Senators/SenatorInfo.js";
-import ErrorPage from "../Pages/ErrorPage.js";
 
+//import About from "../Pages/About.js";
+//import Reps from "../Pages/Representatives/Reps.js";
+//import RepInfo from "../Pages/Representatives/RepInfo.js";
+//import Senators from "../Pages/Senators/Senators.js";
+//import SenatorInfo from "../Pages/Senators/SenatorInfo.js";
+//import ErrorPage from "../Pages/ErrorPage.js";
 
 import classes from "./NavBar.module.css";
+
+const About = React.lazy(() => import('../Pages/About.js'));
+const Reps = React.lazy(() => import('../Pages/Representatives/Reps.js'));
+const RepInfo = React.lazy(() => import('../Pages/Representatives/RepInfo.js'));
+const Senators = React.lazy(() => import('../Pages/Senators/Senators.js'));
+const SenatorInfo = React.lazy(() => import('../Pages/Senators/SenatorInfo.js'));
+const ErrorPage = React.lazy(() => import('../Pages/ErrorPage.js'));
 
 function NavBar() {
 	return (
 		<div>
+			<Suspense fallback={<p>Loading...</p>}>
 			<nav className={classes.NavBar}>
 				<Link to="/" className={classes.Link}>About</Link>
 				<Link to="/representatives" className={classes.Link}>Representatives</Link>
@@ -26,6 +35,7 @@ function NavBar() {
 				<Route path="/senators/:name" element={<SenatorInfo />} />
 				<Route path="*" element={<ErrorPage />} />
 			</Routes>
+			</Suspense>
 		</div>
 	); 
 }
